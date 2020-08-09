5d31200926b1c60e3739afc327f988d1c45843b4
Eugene Chan <eugenechanxz@gmail.com>
2020-08-09 17:08:13 +0800

    pom xml changes

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -9,13 +9,13 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
     //returns datasource object
-    @Bean //we specified it's bean. Spring will look for that bean in the Spring context and replace the DataSource definition with what we've implemented here
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        builder.username("postgres");
-        builder.password("Welcome");
-        System.out.println("My custom datasource bean has been initialized and set");
-        return builder.build();
-    }
+//    @Bean //we specified it's bean. Spring will look for that bean in the Spring context and replace the DataSource definition with what we've implemented here
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        builder.username("postgres");
+//        builder.password("Welcome");
+//        System.out.println("My custom datasource bean has been initialized and set");
+//        return builder.build();
+//    }
 }
